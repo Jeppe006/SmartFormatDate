b7aca2da2562c88e17f5975ed2c308c201f11a73
Jeppe006 <123160036+Jeppe006@users.noreply.github.com>
2023-11-08 11:12:01 +0100

    Add files via upload

---
+++ src/SmartFormatDateModule.ts
@@ -0,0 +1,15 @@
+let SmartFormat : {} = {
+    getDate: function(Language : string, NewDate : Date){
+        let currentDate : Date = new Date
+
+        function _SPLIT_DATE(date : Date){
+            let year = date.getFullYear()
+            let month = date.getMonth()+1
+            let day = date.getDate()
+            return {year,month,day}
+        }
+
+        
+
+    }
+}
